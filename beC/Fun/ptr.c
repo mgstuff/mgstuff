8320101278b71b93d0a21470502350c66da9d0b4
mgstuff <michal.grycki@tieto.com>
2016-06-21 15:21:09 +0200

    Update with ptr funnygunny

--- beC/Fun/ptr.c
+++ beC/Fun/ptr.c
@@ -6,12 +6,21 @@ void
 go_south_east(int* lat,int* lon) {
 
 	*lat = *lat - 1;
-	*lan = *lan +1;
+	*lon = *lon +1;
 
 
 }
 
 
+
+void 
+B(int *worth) {
+
+	*worth = *worth+1;;
+
+
+
+}
 
 int 
 main(void) {
@@ -20,7 +29,28 @@ int latitude = 32;
 int longitude = -64;
 
 go_south_east(&latitude,&longitude );
-printf("%d\t%d", latitude, longitude);
+printf("%d\t%d\n", latitude, longitude);
+
+int z=4;
+int f=8;
+int *ptr;
+int *d_ptr;
+
+ptr = &z;
+printf("%d\n", *ptr);
+
+d_ptr = ptr;
+printf("%d\n", *d_ptr);
+
+z=f;
+
+printf("%d\n", *ptr);
+printf("%d\n", *d_ptr);
+
+
+int networth = 55;
+B(&networth);
+printf("%d\n", networth);
 
 
 return 0;
